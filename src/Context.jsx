fcf7524fdca52addee6a8ef94add14bede03e2c8
rohits-web03 <rohit.dey302003@gmail.com>
2023-09-11 01:23:41 +0530

    Update Context.jsx

--- src/Context.jsx
+++ src/Context.jsx
@@ -10,10 +10,57 @@ export function MyProvider({ children }) {
     const [apiResponse, setApiResponse] = useState('');
     const videoRef = useRef(null);
     const isRecording = useRef(false);
+    const [coordinates,setCoordinates]=useState({lat:'',long:''});
+    const apiBody=useRef({address:'',lat:'',long:''});
+
+    const getLocation = () => {
+      return new Promise((resolve, reject) => {
+        if ('geolocation' in navigator) {
+          navigator.geolocation.getCurrentPosition(
+            async (position) => {
+              try {
+                // Get the user's latitude and longitude
+                const { latitude, longitude } = position.coords;
+    
+                // Set the coordinates state
+                setCoordinates({ lat: latitude, long: longitude });
+                console.log(coordinates);
+                // Reverse geocode the location to get the place name
+                const response = await fetch(
+                  `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+                );
+                const data = await response.json();
+                const placeName = data.display_name;
+    
+                // Set the location state
+                setLocation(placeName);
+                apiBody.current={address:placeName,lat:latitude,long:longitude};
+    
+                // Resolve the Promise with the placeName
+                resolve(placeName);
+              } catch (error) {
+                console.error('Error fetching location:', error);
+                reject(error);
+              }
+            },
+            (error) => {
+              console.error('Error getting location:', error);
+              reject(error);
+            }
+          );
+        } else {
+          console.error('Geolocation is not available in this browser.');
+          reject(new Error('Geolocation is not available.'));
+        }
+      });
+    };
+    
 
     const stopVidRecording = () => {
-        console.log("stop");
+        console.log("stop: ",stream);
+
         if (stream) {
+          console.log("context stop")
           stream.getTracks().forEach(track => track.stop());
           setStream(null);
           videoRef.current.srcObject = null;
@@ -23,7 +70,7 @@ export function MyProvider({ children }) {
 
   // Create an object with the data and functions to provide
   const contextValue = {
-    location,setLocation,stopVidRecording,stream,setStream,apiResponse,setApiResponse,videoRef,isRecording
+    location,setLocation,stopVidRecording,stream,setStream,apiResponse,setApiResponse,videoRef,isRecording,getLocation,coordinates,apiBody
   };
 
   return (
